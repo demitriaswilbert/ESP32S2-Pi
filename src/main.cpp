e65f6b2564e083ac9767daab6d23b3cbe65edac1
demitrias.wilbert <demitriaswilbert@gmail.com>
2024-10-18 18:14:20 +0700

    use GPIO18 as trigger, cleanup, decrease delay

--- src/main.cpp
+++ src/main.cpp
@@ -61,37 +61,27 @@ void setup() {
     pinMode(17, INPUT_PULLUP);
 
     mp_set_memory_functions(psram_malloc, psram_realloc, psram_free);
-    //void begin(unsigned long baud, uint32_t config=SERIAL_8N1, int8_t rxPin=-1, int8_t txPin=-1, bool invert=false, unsigned long timeout_ms = 20000UL, uint8_t rxfifo_full_thrhd = 112);
     Serial0.begin(115200, SERIAL_8N1, 13, 16, true);
     mpzinit(val);
-    pinMode(0, INPUT_PULLUP);
+    pinMode(18, INPUT_PULLUP);
 }
 int inited = 0;
 void loop() {
 
-    // static int64_t last = esp_timer_get_time();
-    // int64_t now = esp_timer_get_time();
-    // if (now - last >= 2000000LL) {
-    //     last = now;
-    // }
-    if (!digitalRead(0)) {
+    
+    if (!(GPIO.in & (1UL << 18))) {
         int64_t time = esp_timer_get_time();
-        euler(val, 1000);
+        chudnovsky(val, 314);
         time = esp_timer_get_time() - time;
 
         char* str = mpz_get_str(NULL, 10, val);
         String pi_str = str;
         free(str);
 
-        // str = pi_str.begin();
-        // size_t pi_len = pi_str.length();
-        // for (size_t i = 0; i < pi_len; i++) 
-        //     str[i] -= '0';
-        
-        Serial.print(pi_str);
-        Serial.printf("\ntime: %lu\ninited:%d\nFree Heap: %lu\nFree PSRAM: %lu\n", (uint32_t)time, inited, ESP.getFreeHeap(), ESP.getFreePsram());
-        Serial.printf("Used Heap: %lu\nUsed PSRAM: %lu\n\n", free_heap, free_psram);
+        // Serial.print(pi_str);
+        // Serial.printf("\ntime: %lu\ninited:%d\nFree Heap: %lu\nFree PSRAM: %lu\n", (uint32_t)time, inited, ESP.getFreeHeap(), ESP.getFreePsram());
+        // Serial.printf("Used Heap: %lu\nUsed PSRAM: %lu\n\n", free_heap, free_psram);
         Serial0.print(pi_str);
     }
-    vTaskDelay(200);
+    vTaskDelay(2);
 }
